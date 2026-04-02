597a712bad1cf7d29b452dc00c3a72e2ae29652b
icey98106 <icey98106@gmail.com>
2026-04-02 15:26:00 +0100

    feat: add requirements.txt + dynamic VITE_API_URL for Render deploy

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,4 +1,4 @@
-export const API = 'http://localhost:8000/api'
+export const API = import.meta.env.VITE_API_URL ?? 'http://localhost:8000/api'
 export const fetcher = (url) =>
   fetch(url).then((r) => {
     if (!r.ok) throw new Error(`${r.status} ${r.statusText}`)
